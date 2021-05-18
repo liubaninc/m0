87011de0d0945c4f6831bdfc0d73015e4f8c3163
Developer Experience team at Tendermint <hello@tendermint.com>
2021-05-18 14:01:15 +0800

    Initialized with Starport

---
+++ cmd/m0d/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	"github.com/liubaninc/m0/app"
+	"github.com/liubaninc/m0/cmd/m0d/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
+		os.Exit(1)
+	}
+}
