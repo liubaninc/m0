3094e72405fdaa0bdadab6f7e400ea35391f2bfc
sid-85 <342529499@qq.com>
2021-05-31 14:54:05 +0800

    wasm

--- x/wasm/types/message_upgrade.go
+++ x/wasm/types/message_upgrade.go
@@ -1,8 +1,10 @@
 package types
 
 import (
+	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	utxotypes "github.com/liubaninc/m0/x/utxo/types"
 )
 
@@ -33,7 +35,35 @@ func (m *MsgUpgrade) GetSigners() []sdk.AccAddress {
 	if err != nil {
 		panic(err)
 	}
-	return []sdk.AccAddress{creator}
+
+	var signers []sdk.AccAddress
+	seen := map[string]bool{}
+	signers = append(signers, creator)
+	seen[m.Creator] = true
+
+	contractInputsMap := map[string]bool{}
+	contractInputs, err := ParseInputsFromExt(m.OutputsExt)
+	if err != nil {
+		panic(err)
+	}
+	for _, contractInput := range contractInputs {
+		contractInputsMap[contractInput.Index()] = true
+	}
+
+	for _, input := range m.Inputs {
+		if _, ok := contractInputsMap[input.Index()]; ok {
+			continue // 忽略合约内部转账的地址
+		}
+		if addrStr := input.FromAddr; !seen[addrStr] {
+			addr, err := sdk.AccAddressFromBech32(input.FromAddr)
+			if err != nil {
+				panic(err)
+			}
+			signers = append(signers, addr)
+			seen[addrStr] = true
+		}
+	}
+	return signers
 }
 
 func (m *MsgUpgrade) GetSignBytes() []byte {
@@ -47,28 +77,94 @@ func (m *MsgUpgrade) ValidateBasic() error {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
 	}
 
-	if len(m.ContractRequests) == 0 {
-
-	}
-
 	totalIn := sdk.NewCoins()
+	inputsMap := map[string]bool{}
 	for _, input := range m.Inputs {
 		if err := input.ValidateBasic(); err != nil {
 			return err
 		}
+		key := fmt.Sprintf("%s_%d_%d", input.RefTx, input.RefMsg, input.RefOffset)
+		if _, ok := inputsMap[input.Index()]; ok {
+			return sdkerrors.Wrapf(utxotypes.ErrUTXODuplicated, "invalid spend utxo %s (%s)", key, err)
+		}
+		inputsMap[input.Index()] = true
 		totalIn = totalIn.Add(input.Amount)
 	}
 
 	totalOut := sdk.NewCoins()
+	outputsMap := map[string]int{}
 	for _, output := range m.Outputs {
 		if err := output.ValidateBasic(); err != nil {
 			return err
 		}
+		key := fmt.Sprintf("%s_%s", output.ToAddr, output.Amount)
+		outputsMap[key]++
 		totalOut = totalOut.Add(output.Amount)
 	}
 
-	if !totalIn.IsEqual(totalOut) {
-		return sdkerrors.Wrapf(utxotypes.ErrInputOutputNotEqual, "inputs amount %v outputs amount %v", totalIn, totalOut)
+	if !totalIn.IsAllLTE(totalOut) {
+		return sdkerrors.Wrapf(utxotypes.ErrInputOutputNotLitter, "inputs amount %v outputs amount %v", totalIn, totalOut)
+	}
+
+	if len(m.ContractRequests) == 0 {
+		return sdkerrors.Wrapf(ErrUnexpected, "contract request is empty")
+	}
+
+	if len(m.OutputsExt) != 0 {
+		// 读集合 key列表
+		inputExtKeys := map[string]bool{}
+		for _, inputExt := range m.InputsExt {
+			key := fmt.Sprintf("%s_%s", inputExt.Bucket, inputExt.Key)
+			if _, ok := inputExtKeys[key]; ok {
+				return sdkerrors.Wrapf(ErrUnexpected, "duplicate bucket key in input ext")
+			}
+			inputExtKeys[key] = true
+		}
+		// 写集合key 必须 存在读集合key list
+		for _, outputExt := range m.OutputsExt {
+			if outputExt.Bucket == TransientBucket {
+				continue
+			}
+			key := fmt.Sprintf("%s_%s", outputExt.Bucket, outputExt.Key)
+			if _, ok := inputExtKeys[key]; !ok {
+				return sdkerrors.Wrapf(ErrUnexpected, "output ext not such key %s in input ext", key)
+			}
+			if outputExt.Value == nil {
+				return sdkerrors.Wrapf(ErrUnexpected, "output ext value is null")
+			}
+		}
+
+		contractInputs, err := ParseInputsFromExt(m.OutputsExt)
+		if err != nil {
+			return sdkerrors.Wrapf(ErrUnexpected, "parse contract inputs error - %v", err)
+		}
+		for _, contractInput := range contractInputs {
+			key := fmt.Sprintf("%s_%d_%d", contractInput.RefTx, contractInput.RefMsg, contractInput.RefOffset)
+			if _, ok := inputsMap[key]; !ok {
+				return sdkerrors.Wrapf(ErrUnexpected, "missing contract input in inputs")
+			}
+		}
+
+		contractOutputs, err := ParseOutputsFromExt(m.OutputsExt)
+		if err != nil {
+			return sdkerrors.Wrapf(ErrUnexpected, "parse contract outputs error - %v", err)
+		}
+		for _, request := range m.ContractRequests {
+			for _, amount := range request.Amount {
+				contractOutputs = append(contractOutputs, &utxotypes.Output{
+					ToAddr: authtypes.NewModuleAddress(request.ContractName).String(),
+					Amount: amount,
+				})
+			}
+		}
+		for _, contractOutput := range contractOutputs {
+			key := fmt.Sprintf("%s_%s", contractOutput.ToAddr, contractOutput.Amount)
+			if val, ok := outputsMap[key]; !ok || val < 1 {
+				return sdkerrors.Wrapf(ErrUnexpected, "missing contract output in outputs")
+			} else {
+				outputsMap[key] = val - 1
+			}
+		}
 	}
 	return nil
 }
