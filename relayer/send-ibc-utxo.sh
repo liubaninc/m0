541ac966484e80b1f6500a8783e2a6a8b7f3c5fa
sid-85 <342529499@qq.com>
2021-08-25 17:45:03 +0800

    f

--- relayer/send-ibc-utxo.sh
+++ relayer/send-ibc-utxo.sh
@@ -5,7 +5,7 @@ echo "send-ibc-utxo... earth ==> mars"
 echo "before"
 echo "earth chain balances $(m0d q bank balances $(m0d keys show alice -a --home ~/.earth/) --output json)"
 echo "mars chain balances $(m0d q bank balances $(m0d keys show alice -a --home ~/.mars/) --output json  --node tcp://127.0.0.1:26659)"
-m0d tx mibc send-ibc-utxo mibc channel-0 $(m0d keys show alice -a --home ~/.mars) 13m0token --from alice --home ~/.earth --chain-id earth -y
+echo "hash $(m0d tx mibc send-ibc-utxo mibc channel-0 $(m0d keys show alice -a --home ~/.mars) 13m0token --from alice --home ~/.earth --chain-id earth -y | jq .txhash)"
 sleep 60
 echo "after"
 echo "earth chain  balances $(m0d q bank balances $(m0d keys show alice -a --home ~/.earth/) --output json)"
@@ -17,7 +17,7 @@ echo "send-ibc-utxo... mars==> earth"
 echo "before"
 echo "earth chain balances $(m0d q bank balances $(m0d keys show alice -a --home ~/.earth/) --output json)"
 echo "mars chain balances $(m0d q bank balances $(m0d keys show alice -a --home ~/.mars/) --output json  --node tcp://127.0.0.1:26659)"
-m0d tx mibc send-ibc-utxo mibc channel-0 $(m0d keys show alice -a --home ~/.earth) 13$t --from alice --home ~/.mars --chain-id mars -y --node tcp://127.0.0.1:26659
+echo "hash $(m0d tx mibc send-ibc-utxo mibc channel-0 $(m0d keys show alice -a --home ~/.earth) 13$t --from alice --home ~/.mars --chain-id mars -y --node tcp://127.0.0.1:26659 | jq .txhash)"
 sleep 60
 echo "after"
 echo "earth chain  balances $(m0d q bank balances $(m0d keys show alice -a --home ~/.earth/) --output json)"
